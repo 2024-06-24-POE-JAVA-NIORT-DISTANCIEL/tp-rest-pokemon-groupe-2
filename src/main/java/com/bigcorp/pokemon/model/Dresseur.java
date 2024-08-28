707919ea8a99e105b8adc42c3de902aa4763d125
Hugo Bossard <hbossard01@gmail.com>
2024-08-28 14:44:14 +0200

    Ajout : Classe Objet et Dresseur sans liens entre eux

--- src/main/java/com/bigcorp/pokemon/model/Dresseur.java
+++ src/main/java/com/bigcorp/pokemon/model/Dresseur.java
@@ -1,4 +1,51 @@
 package com.bigcorp.pokemon.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Dresseur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String pseudonyme;
+
+    private String motDePasse;
+
+    private Integer portefeuille;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPseudonyme() {
+        return pseudonyme;
+    }
+
+    public void setPseudonyme(String pseudonyme) {
+        this.pseudonyme = pseudonyme;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public Integer getPortefeuille() {
+        return portefeuille;
+    }
+
+    public void setPortefeuille(Integer portefeuille) {
+        this.portefeuille = portefeuille;
+    }
 }
