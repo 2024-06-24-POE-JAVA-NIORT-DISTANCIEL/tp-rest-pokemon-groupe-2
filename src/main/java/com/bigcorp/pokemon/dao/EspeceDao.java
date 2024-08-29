b9a6a3f2ed9eb4369abd91725831f357f0d009d5
Yannis <yannis_hammou@hotmail.fr>
2024-08-29 14:40:47 +0200

    Modification EspeceDao

--- src/main/java/com/bigcorp/pokemon/dao/EspeceDao.java
+++ src/main/java/com/bigcorp/pokemon/dao/EspeceDao.java
@@ -12,4 +12,10 @@ public interface EspeceDao extends CrudRepository<Espece,Integer> {
      * Recherche tous les pokemons par leur nom
      */
     public List<Espece> findByNom(String nom);
+
+    /**
+     * Recherche tous les pokemons par leur nompar , en ne tenant pas compte de la casse
+     */
+    public List<Espece> findByNomIgnoreCase(String nom);
+
 }
--- src/test/java/com/bigcorp/pokemon/dao/TestEspeceDao.java
+++ src/test/java/com/bigcorp/pokemon/dao/TestEspeceDao.java
@@ -1,11 +1,14 @@
 package com.bigcorp.pokemon.dao;
 
 import com.bigcorp.pokemon.model.Espece;
+import com.bigcorp.pokemon.model.Pokemon;
 import com.bigcorp.pokemon.model.Type;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class TestEspeceDao {
@@ -26,6 +29,54 @@ public class TestEspeceDao {
     System.out.println("Après save");
     Assertions.assertNotNull(savedEspece.getId());
     Assertions.assertEquals("bipède", savedEspece.getNom());
+    }
+
+    @Test
+    public void testEspecefindByNom(){
+
+        // Préparation des données
+        Espece e1 = new Espece();
+        e1.setNom("Volatile");
+        especeDao.save(e1);
+
+        Espece e2 = new Espece();
+        e2.setNom("Canide");
+        especeDao.save(e2);
+
+        Espece e3 = new Espece();
+        e3.setNom("Félin");
+        especeDao.save(e3);
+
+        // Exécution de la méthode à tester
+        List<Espece> result = especeDao.findByNom("Félin");
+
+        // Vérification des résultats
+        Assertions.assertFalse(result.isEmpty());
+        System.out.println(result);
+    }
+
+    @Test
+    public void testEspecefindByNomContainingIgnoreCase(){
+
+        // Préparation des données
+        Espece e1 = new Espece();
+        e1.setNom("Volatile");
+        especeDao.save(e1);
+
+        Espece e2 = new Espece();
+        e2.setNom("Canide");
+        especeDao.save(e2);
+
+        Espece e3 = new Espece();
+        e3.setNom("Félin");
+        especeDao.save(e3);
+
+        // Exécution de la méthode à tester
+        List<Espece> result = especeDao.findByNomIgnoreCase("Fél");
+
+        // Vérification des résultats
+        Assertions.assertFalse(result.isEmpty());
+        System.out.println(result);
     }
 
 }
