a1521ea434b4027d07edf53ac24adee4e2a90569
Yannis <yannis_hammou@hotmail.fr>
2024-08-30 11:19:21 +0200

    Ajout d'un GET dans PokemonController et modif DiscussionPokemonService

--- src/main/java/com/bigcorp/pokemon/service/DiscussionPokemonService.java
+++ src/main/java/com/bigcorp/pokemon/service/DiscussionPokemonService.java
@@ -26,11 +26,20 @@ public class DiscussionPokemonService {
 
         Pokemon pokemon = optionalPokemon.get();
         String nom = pokemon.getNom();
+
         Random random = new Random(); //Un générateur de random
 
         // Répéter le nom 1 à 3 fois
         int repeatCount = random.nextInt(3) + 1; // le fait 1 fois puis répète entre 1 à 3
-        String message = nom.repeat(repeatCount);
+        String message = "" ;
+        for (int i = 0; i < repeatCount; i++){
+            if (i < repeatCount - 1) {
+                message += nom + " ";
+            }
+            else {
+                message += nom;
+            }
+        }
 
         // Ajouter un signe de ponctuation aléatoire
         char[] punctuations = {'.', '!', '?'};
