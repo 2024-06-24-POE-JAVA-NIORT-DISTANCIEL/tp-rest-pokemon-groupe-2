d6778337139eb31dfeca75ab8b0ebb4cb1ecffde
Barzan <barzanartemiss@gmail.com>
2024-09-02 15:14:25 +0200

    AjoutDTO&MAPPER

--- src/main/java/com/bigcorp/pokemon/dto/AttaqueDTO.java
+++ src/main/java/com/bigcorp/pokemon/dto/AttaqueDTO.java
@@ -1,5 +1,38 @@
 package com.bigcorp.pokemon.dto;
 
-public class AttaqueDTO {
+import com.bigcorp.pokemon.model.Type;
+
+
+public class AttaqueDTO { 
+    private Integer id;
+    private String nom;
+    private Type type;
+    private Integer pointsDegats;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public Type getType() {
+        return type;
+    }
+    public void setType(Type type) {
+        this.type = type;
+    }
+    public Integer getPointsDegats() {
+        return pointsDegats;
+    }
+    public void setPointsDegats(Integer pointsDegats) {
+        this.pointsDegats = pointsDegats;
+    }
+
+    
 
 }
